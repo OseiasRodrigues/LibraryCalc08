61388745341420d859b0623b1b6b5fe021c34d09
Oseias Rodrigues <oseiassrodrigues@outlook.com>
2019-07-13 12:18:10 -0300

    Adicionando operacao

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LibraryCalc08
+{
+    public class Calculator : ICalculator
+    {
+        public int Adicao (int x, int y)
+        {
+            return x + y;
+        }
+        
+        public int Subtracao(int x, int y)
+        {
+            return x - y;
+        }
+
+      
+        public float Divisao(int x, int y)
+        {
+            return (float)x / y;
+        }
+
+        public int Multiplicacao(int x, int y)
+        {
+            return (x * y);
+        }
+        public int Adicao(int x, int y, int z) /* Adicao de tres valores */
+
+        {
+            return (x + y + z) ;
+        }
+    }
+    
+
+}
+
+
---
+++ ICalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryCalc08
+{
+    interface ICalculator
+    {
+        // <Soma de valores Inteiros>
+        int Adicao(int x, int y);
+
+        // <Subtrai valores  inteiros>
+        int Subtracao(int x, int y);
+
+        // <Multiplica os Valores Inteiros>
+        int Multiplicacao(int x, int y);
+
+        // <Divide os valores reais>
+        float Divisao(int x, int y);
+
+        // <Soma de Tres fatores Inteiros>
+        int Adicao(int x, int y, int z);
+    }
+}
